5df0e94b6149601851610ec365d4ed58d1316706
George <gnjuki19@gmail.com>
2024-07-11 13:28:30 +0300

    Able to fetch journals, add them and better UI for them.

--- src/components/CategoryDetails.jsx
+++ src/components/CategoryDetails.jsx
@@ -4,6 +4,7 @@ import { Link, useParams } from "react-router-dom";
 const CategoryDetails = () => {
   const { ID } = useParams();
   const [catDetails, setCatDetails] = useState({});
+  // const [journals, setJournals] = useState([]);
   const token = localStorage.getItem("token");
   if (!token) throw new Error("No token found");
 
@@ -22,13 +23,15 @@ const CategoryDetails = () => {
         return response.json();
       })
       .then((data) => {
-        console.log(data);
-        setCatDetails(data);
+        console.log(data.Category);
+        setCatDetails(data.Category);
+        // console.log("THis is the journals ", data.Category.Journals);
+        // setJournals(data.Category.Journals);
       })
       .catch((error) => {
         console.error("There was an error fetching the categories!", error);
       });
-  }, [token]);
+  }, [token, ID]);
   return (
     <section class="bg-white dark:bg-gray-900">
       <div className="flex justify-end mb-4">
@@ -36,13 +39,13 @@ const CategoryDetails = () => {
           to={`/categories/${ID}/journals/create`}
           className="bg-green-500 text-white font-bold py-2 px-4 rounded"
         >
-          Add a Journal Entry +
+          Add {catDetails.Title} +
         </Link>
       </div>
       <div class="container px-6 py-10 mx-auto">
         <div class="text-center">
           <h1 class="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">
-            From the blog
+            {catDetails.Title}
           </h1>
 
           <p class="max-w-lg mx-auto mt-4 text-gray-500">
@@ -50,69 +53,51 @@ const CategoryDetails = () => {
             tasty liquor. Sweet savory foodtruck pie.
           </p>
         </div>
-        //////////////////////////////////////////////
-        {!catDetails ? (
-          <div>
-            <h1>THere are no details of {catDetails.Title}</h1>
-            <div className="flex justify-end mb-4">
-              <Link
-                to={`/categories/${ID}/journals/create`}
-                className="bg-green-500 text-white font-bold py-2 px-4 rounded"
-              >
-                Add a Journal Entry +
-              </Link>
-            </div>
-          </div>
-        ) : (
-          <div class="grid grid-cols-1 gap-8 mt-8 md:mt-16 md:grid-cols-2 xl:grid-cols-3">
-            <div>
-              <div class="relative">
-                <img
-                  class="object-cover object-center w-full h-64 rounded-lg lg:h-80"
-                  src="https://images.unsplash.com/photo-1624996379697-f01d168b1a52?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-                  alt=""
-                />
-
-                <div class="absolute bottom-0 flex p-3 bg-white dark:bg-gray-900 ">
+        {/* vfghcdjnk */}
+        <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+          {catDetails.Journals ? (
+            catDetails.Journals.map((journal) => (
+              <div>
+                <div class="relative">
                   <img
-                    class="object-cover object-center w-10 h-10 rounded-full"
-                    src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                    class="object-cover object-center w-full h-64 rounded-lg lg:h-80"
+                    src={journal.ImageUrl}
                     alt=""
                   />
+                </div>
 
-                  <div class="mx-4">
-                    <h1 class="text-sm text-gray-700 dark:text-gray-200">
-                      Tom Hank
-                    </h1>
-                    <p class="text-sm text-gray-500 dark:text-gray-400">
-                      Creative Director
-                    </p>
-                  </div>
+                <h1 class="mt-6 text-xl font-semibold text-gray-800 dark:text-white">
+                  {journal.Title}
+                </h1>
+
+                <hr class="w-32 my-6 text-blue-500" />
+
+                <p class="text-sm text-gray-500 dark:text-gray-400">
+                  {journal.Content.substring(0, 200)} ...
+                </p>
+
+                <a
+                  href="#"
+                  class="inline-block mt-4 text-blue-500 underline hover:text-blue-400"
+                >
+                  Read more?
+                </a>
+              </div>
+            ))
+          ) : (
+            <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+              {" "}
+              <div>
+                <div class="relative">
+                  {" "}
+                  <p>No journal entries found.</p>
                 </div>
               </div>
-
-              <h1 class="mt-6 text-xl font-semibold text-gray-800 dark:text-white">
-                What do you want to know about UI
-              </h1>
-
-              <hr class="w-32 my-6 text-blue-500" />
-
-              <p class="text-sm text-gray-500 dark:text-gray-400">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Blanditiis fugit dolorum amet dolores praesentium, alias nam?
-                Tempore
-              </p>
-
-              <a
-                href="#"
-                class="inline-block mt-4 text-blue-500 underline hover:text-blue-400"
-              >
-                Read more
-              </a>
             </div>
-          </div>
-        )}
-        //////////////////////////////////////////////
+          )}
+        </div>
+
+        {/* cg jhbn */}
       </div>
     </section>
   );
