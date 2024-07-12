25cc185a73df1f28a4dc1559549375d0b80b98dc
George <gnjuki19@gmail.com>
2024-07-12 16:08:45 +0300

    Cleaning up pages.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
             Share your thoughts, feelings and{" "}
             <span className="text-blue-400">Ideas</span> today
           </h1>
-          <button className="w-full px-5 py-2 mt-4 text-sm font-medium text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+          <button className="w-full px-5 py-2 mt-4 text-sm font-medium text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500" >
             Start project
           </button>
         </div>
